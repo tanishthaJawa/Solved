f6cae5193e7a369662fdde819fffb33be226e72c
tanJ08 <81076795+tanJ08@users.noreply.github.com>
2021-09-12 13:46:52 +0530

    Update firstAndLastOccurence.java

--- sheetSolutions/searchSort/firstAndLastOccurence.java
+++ sheetSolutions/searchSort/firstAndLastOccurence.java
@@ -1,59 +1,83 @@
 package sheetSolutions.searchSort;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+/*
+finds the first and last occurrence in sorted array
+ */
+import java.util.Arrays;
 
-public class firstAndLastOccurence {
-    ArrayList<Long> find(long[] arr, int n, int x) {
-        // code here
-        return binarySearch(arr, x);
-////        HashMap<Long,HashMap<Integer,Integer>> hs = new HashMap<>();
-////        for (int i = 0; i < n; i++) {
-////            if(hs.containsKey(arr[i])){
-////                hs.put(arr[i],new Integer(i,hs.get(arr[i]);
-////            }
-////            else{
-////                hs.put(arr[i],1);
-////            }
-////        }
+public class firstAndLastOccurrence {
+    //recursive approach .Takes O(log N) time and O(log n) space
+   static int first(int []ar,int low,int high, int x){
+    int mid;
+       if (low<=high){
+           mid=low+(high-low)/2;
+           if( (mid==0 || ar[mid-1]<x) && ar[mid]==x )
+               return mid;
+           else if(x > ar[mid])
+               return first(ar,mid+1,high,x);
+           else
+               return  first(ar,low,mid-1,x);
+       }
+       return -1;
    }
-        private ArrayList<Long> binarySearch ( long[] arr, int x){
-            int mid,low=0,high= arr.length;
-            int midR, midL;
-            ArrayList<Long> index = new ArrayList<>();
-            index.add((long) -1);
-            index.add((long) -1);
-            while (low <= high) {
-                mid = low + (high-low) / 2;
-                if (arr[mid] == x) {
-                    midR = mid ;
-                    midL = mid ;
-                    while (arr[midR + 1] == x) {
-                        midR++;
-                    }
-                    while (arr[midL - 1] == x) {
-                        midL--;
-                    }
-                    index.set(0, (long) midL);
-                    index.set(1, (long) midR);
-                    return index;
-                } else if (arr[mid] < x) {
-                    low = mid+1;
-                } else {
-                   high=mid-1;
-                }
+    static int last(int []ar,int low,int high, int x){
+        if (low<=high){
+           int mid=low+(high-low)/2;
+            if((mid==ar.length-1 || ar[mid+1]>x) && ar[mid]==x){
+                return mid;
             }
-            return index;
-
+            else if(x<ar[mid])
+                return last(ar,low,mid-1,x);
+            else
+                return  last(ar,mid+1,high,x);
         }
+        return -1;
+    }
+    static int first1(int []ar,int low,int high, int x){
+       //iterative approach .Takes O(log N) time and O(1) space
+        int mid,res=-1;
+        while (low<=high){
+            mid=low+(high-low)/2;
+            // If arr[mid] is same as
+            // x, we update res and
+            // move to the left half.
+            if( ar[mid]==x ){
+                res=mid;
+                high=mid-1;
+            }
+            else if(x < ar[mid])
+               high=mid-1;
+            else
+                low=mid+1;
+        }
+        return res;
+    }
+    static int last1(int []ar,int low,int high, int x){
+       int res=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            // If arr[mid] is same as
+            // x, we update res and
+            // move to the right half.
+            if(ar[mid]==x){
+                res= mid;
+                low=mid+1;
+            }
+            else if(x<ar[mid])
+                high=mid-1;
+            else
+              low=mid+1;
+        }
+        return res;
+    }
 
     public static void main(String[] args) {
-       long[] arr = { 1, 3, 5, 5, 5, 5, 7, 123, 125 };
-       firstAndLastOccurence ob = new firstAndLastOccurence();
-       ArrayList<Long> hs =ob.find(arr,9,7);
-        for (Long h : hs) {
-            System.out.println(h);
-        }
+        int [] arr={2,3,5,5,6,6,7,7,7,7,8,8};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(first(arr,0,arr.length-1,7));
+        System.out.println(last(arr,0,arr.length-1,7));
+        System.out.println(first1(arr,0,arr.length-1,7));
+        System.out.println(last1(arr,0,arr.length-1,7));
     }
-}
 
+}
