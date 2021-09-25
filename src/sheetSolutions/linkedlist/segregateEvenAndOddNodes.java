6f32c0102723c8477c1e4752407d2caab6148cc0
tanJ08 <tanishtha08@gmail.com>
2021-09-25 20:18:01 +0530

    committed

--- src/sheetSolutions/linkedlist/segregateEvenAndOddNodes.java
+++ src/sheetSolutions/linkedlist/segregateEvenAndOddNodes.java
@@ -1,4 +1,48 @@
 package sheetSolutions.linkedlist;
 
 public class segregateEvenAndOddNodes {
+    static Node divide(Node head) {
+        Node evenStart = null;
+        Node evenEnd = null;
+        Node oddStart = null;
+        Node oddEnd = null;
+        Node currentNode = head;
+
+        while (currentNode != null) {
+            int element = currentNode.value;
+
+            if (element % 2 == 0) {
+
+                if (evenStart == null) {
+                    evenStart = currentNode;
+                    evenEnd = evenStart;
+                } else {
+                    evenEnd.next = currentNode;
+                    evenEnd = evenEnd.next;
+                }
+
+            } else {
+
+                if (oddStart == null) {
+                    oddStart = currentNode;
+                    oddEnd = oddStart;
+                } else {
+                    oddEnd.next = currentNode;
+                    oddEnd = oddEnd.next;
+                }
+            }
+            // Move head pointer one step in forward direction
+            currentNode = currentNode.next;
+        }
+
+
+        if (oddStart == null || evenStart == null) {
+            return head;
+        }
+
+        evenEnd.next = oddStart;
+        oddEnd.next = null;
+        return evenStart;
+    }
 }
+
