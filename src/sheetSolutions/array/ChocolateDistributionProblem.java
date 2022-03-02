e5934bfca5c109d3cf4c24ae0ea35fb20510652e
Tanishtha <tanishtha08@gmail.com>
2022-03-02 19:12:33 +0530

    commits

---
+++ src/sheetSolutions/array/ChocolateDistributionProblem.java
@@ -0,0 +1,52 @@
+package sheetSolutions.array;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+Given an array A[ ] of positive integers of size N, where each value represents the number of chocolates in a packet. Each packet
+ can have a variable number of chocolates. There are M students, the task is to distribute chocolate packets among M students such
+  that :
+1. Each student gets exactly one packet.
+2. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is
+minimum.
+
+Example 1:
+
+Input:
+N = 8, M = 5
+A = {3, 4, 1, 9, 56, 7, 9, 12}
+Output: 6
+Explanation: The minimum difference between
+maximum chocolates and minimum chocolates
+is 9 - 3 = 6 by choosing following M packets :
+{3, 4, 9, 7, 9}.
+ */
+public class ChocolateDistributionProblem {
+  /*
+  Time complexity:O(nlogn)
+  The idea is to use sorting.
+  A simple solution is to generate all subsets of size m of arr[0..n-1]. For every subset, find the difference between the maximum and
+  minimum elements in it. Finally, return the minimum difference. An efficient solution is based on the observation that to
+   minimize the difference, we must choose consecutive elements from a  sorted packet. We first sort the array arr[0..n-1], then
+    find the subarray of size m with the minimum difference between the last and first elements.
+   */
+  static long findMinDiff(ArrayList<Long> a, long n, long m) {
+    // if there are no chocolates(n) or number of students (m)is 0
+    if (m == 0 || n == 0) {
+      return 0;
+    }
+    Collections.sort(a);
+    if (n < m) { // Number of students cannot be more than number of packets
+      return -1;
+    }
+    long minDiff = Long.MAX_VALUE;
+    for (int i = 0; i + m - 1 < n; i++) {
+      long diff = a.get(i + (int) m - 1) - a.get(i);
+      if (diff < minDiff) {
+        minDiff = diff;
+      }
+    }
+    return minDiff;
+  }
+}
---
+++ src/sheetSolutions/array/MinOperationsToMakeArrayPalindrome.java
@@ -0,0 +1,66 @@
+package sheetSolutions.array;
+/*
+Given an array of positive integers. We need to make the given array a ‘Palindrome’. The only allowed operation is”merging” (of two
+adjacent elements). Merging two adjacent elements means replacing them with their sum. The task is to find the minimum number of
+merge operations required to make the given array a ‘Palindrome’.To make any array a palindrome, we can simply apply a merge
+operation n-1 times where n is the size of the array (because a single-element array is always palindromic, similar to
+single-character string). In that case, the size of array will be reduced to 1. But in this problem, we are asked to do it in
+the minimum number of operations.
+
+Example :
+
+Input : arr[] = {15, 4, 15}
+Output : 0
+Array is already a palindrome. So we
+do not need any merge operation.
+
+Input : arr[] = {1, 4, 5, 1}
+Output : 1
+We can make given array palindrome with
+minimum one merging (merging 4 and 5 to
+make 9)
+
+Input : arr[] = {11, 14, 15, 99}
+Output : 3
+We need to merge all elements to make
+a palindrome.
+ */
+public class MinOperationsToMakeArrayPalindrome {
+  /*
+   Let f(i, j) be minimum merging operations to make subarray arr[i..j] a palindrome. If i == j answer is 0. We start i from 0 and
+    j from n-1.
+
+  1.If arr[i] == arr[j], then there is no need to do any merging operations at index i or index j. Our answer in this case will be
+       f(i+1, j-1).
+  2.Else, we need to do merging operations. Following cases arise.
+      1.If arr[i] > arr[j], then we should do merging operation at index j. We merge index j-1 and j, and update arr[j-1] = arr[j-1] +
+           arr[j]. Our answer in this case will be 1 + f(i, j-1).
+      2.For the case when arr[i] < arr[j], update arr[i+1] = arr[i+1] + arr[i]. Our answer in this case will be 1 + f(i+1, j).
+  3.Our answer will be f(0, n-1), where n is the size of array arr[].
+       */
+  static int minOperations(int[] arr, int n) {
+    int count = 0;
+    for (int i = 0, j = n - 1;
+        i <= j; ) { // no need to update counter as it is already updated in loop
+      // If corner elements are same no need for merging
+      if (arr[i] == arr[j]) {
+        i++;
+        j--;
+      } else if (arr[i] > arr[j]) { // we merge at the right end
+        j--;
+        arr[j] += arr[j + 1]; // does not give exception because counter is already decremented.
+        count++;
+      } else { // we merge at left end
+        i++;
+        arr[i] += arr[i - 1];
+        count++;
+      }
+    }
+    return count;
+  }
+
+  public static void main(String[] args) {
+    int[] arr = {11, 14, 15, 99};
+    System.out.println(minOperations(arr, arr.length));
+  }
+}
