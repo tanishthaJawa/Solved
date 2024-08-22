fabb2fb2df0956b1a0fcc92189d5f9aa5659f983
Tanishtha Jawa <81076795+tanishthaJawa@users.noreply.github.com>
2024-08-22 20:39:42 +0530

    Update BinaryTreeFromPreorderAndInorder.java

--- src/sheetSolutions/binaryTree/BinaryTreeFromPreorderAndInorder.java
+++ src/sheetSolutions/binaryTree/BinaryTreeFromPreorderAndInorder.java
@@ -25,6 +25,41 @@ public class BinaryTreeFromPreorderAndInorder {
         root.right = buildTree(preorder, preStartIdx + nodesToTheLeftIdx +1, preEndIdx, rootIdx + 1, inEndIdx, inorderNodes);
         return root;
     }
+     // Method 2
+
+     public static Node buildTree(int inorder[], int preorder[], int n)
+    {
+        // Map has no role in this question 
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int i=0;
+        for(int x:inorder){
+            map.put(x,i++);
+        }
+        Node root = BuildBT(preorder,0,n-1,inorder,0,n-1,map);
+        return root;
+    }
+    public static Node BuildBT(int[] preorder,int preStart, int preEnd, int[] inorder, int inStart, int inEnd, HashMap<Integer,Integer> map){
+        if(preStart>preEnd || inStart>inEnd) return null;
+        
+        Node root = new Node(preorder[preStart]);
+        // int index = map.get(root.data);
+        int index = get_pos(inorder,inStart, inEnd, root.data);
+
+        int nElme = index - inStart;
+        
+        root.left = BuildBT(preorder, preStart+1, preStart+nElme, inorder, inStart, index-1, map);
+        root.right = BuildBT(preorder, preStart+nElme+1, preEnd, inorder, index+1, inEnd, map);
+        return root;
+    }
+    // No. are duplicate so we have to find the index in range of inStart-inEnd
+    public static int get_pos(int arr[], int start, int end, int val)
+    {
+        for(int i=start; i<=end; i++)
+        {
+            if(arr[i]==val) return i;
+        }
+        return -1;
+    }
 
     private void preOrder(Node root) {
         if (root == null) {
