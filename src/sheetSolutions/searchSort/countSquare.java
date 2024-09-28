ceaf05d83f9f05d96a8c6922846d6c73b2e95a18
Tanishtha Jawa <81076795+tanishthaJawa@users.noreply.github.com>
2024-09-28 17:45:03 +0530

    Update countSquare.java

--- src/sheetSolutions/searchSort/countSquare.java
+++ src/sheetSolutions/searchSort/countSquare.java
@@ -9,7 +9,29 @@ public class countSquare {
     }
     return count;
   }
+  
+static int countSquares(int N) {
+        // code here
+       if (N == 0 || N == 1)
+            return 0;
 
+        long start = 1, end= N/2, ans = 0;
+        while (start <= end) {
+            long mid = start + (end - start) / 2;
+
+            // If x is a perfect square
+            if (mid * mid == N)
+                return (int) mid -1;
+
+            if (mid * mid < N) {
+                start = mid + 1;
+                ans = mid;
+            }
+            else // If mid*mid is greater than x
+                end = mid - 1;
+        }
+        return (int)ans;
+    }
   public static void main(String[] args) {
     System.out.println(count(9));
   }
