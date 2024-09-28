8fc1c2c1e457f150778c01ad5ca174689c5dd030
Tanishtha Jawa <81076795+tanishthaJawa@users.noreply.github.com>
2024-09-28 16:23:35 +0530

    Update searchInRotatedArray.java

--- src/sheetSolutions/searchSort/searchInRotatedArray.java
+++ src/sheetSolutions/searchSort/searchInRotatedArray.java
@@ -5,6 +5,56 @@ import java.util.Arrays;
 public class searchInRotatedArray {
 
   // Complete this function
+    public static int searchBS(int[] nums, int target) {
+        int start = 0, end = nums.length -1;
+        int mid = -1, prevEle = 0, nextEle = 0;
+        int minIndex =0;
+      // Array already sorted
+        if(nums[start]<=nums[end]){
+            return binarySearch(nums,0,end,target);
+        }
+        while(start<=end){
+            mid = (start+end)/2;
+            prevEle = (mid+nums.length -1)%nums.length;
+            nextEle = (mid+1)%nums.length;
+            if(nums[mid] <= nums[prevEle] && nums[mid]<= nums[nextEle]){
+                minIndex = mid;
+                break;
+            }
+            else if(nums[0]<=nums[mid]){
+                start = mid +1;
+            }
+            else{
+                end = mid -1;
+            }
+        }
+          
+          if(target>=nums[0]){
+           return  binarySearch(nums, 0, minIndex -1, target);
+          }
+          else{
+             return binarySearch(nums, minIndex,nums.length -1, target);
+          }
+    }
+
+    public int binarySearch(int[] arr , int low, int high, int target){
+        int mid = 0;
+        while(low<=high){
+             mid = low+(high -low)/2;
+            if(arr[mid]== target){
+                return mid;
+            }
+            else if(arr[mid]<target){
+                low = mid+1;
+            }
+            else{
+                high = mid -1;
+            }
+        }
+        return -1;
+    }
+
+  
   static int search(int[] nums, int target) {
     int left = 0, right = nums.length - 1;
     // First step is to find the pivot where the
