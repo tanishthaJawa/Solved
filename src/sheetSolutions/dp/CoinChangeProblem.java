8aa753414a2f6675fcf8f541becd6fa16d36d58c
Tanishtha Jawa <81076795+tanishthaJawa@users.noreply.github.com>
2025-01-19 20:00:14 +0530

    Update CoinChangeProblem.java

--- src/sheetSolutions/dp/CoinChangeProblem.java
+++ src/sheetSolutions/dp/CoinChangeProblem.java
@@ -32,6 +32,34 @@ public class CoinChangeProblem {
     return table[n]; // last value indicates no. of ways
   }
 
+    public int count(int coins[], int sum) {
+        // code here.
+        int n = coins.length;
+        int [][] dp = new int[n+1][sum+1];
+        for(int i =0;i<=n;i++){
+            for(int j=0;j<=sum;j++){
+                if(i==0){
+                    dp[i][j]=0;
+                }
+                if(j==0){
+                    dp[i][j]=1;
+                }
+            }
+        }
+        
+        for(int i =1;i<=n;i++){
+            for(int j =1;j<=sum;j++){
+                if(coins[i-1]<=j){
+                    dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
+    }
+
   public static void main(String[] args) {
     //
     int[] arr = {1, 2, 3};
